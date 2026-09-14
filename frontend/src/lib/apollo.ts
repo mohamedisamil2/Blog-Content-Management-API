be36dd89eea991a844f16704d07fcd18097ac23b
mohamedismail2 <mohameismail20@gmail.com>
2026-09-14 19:35:53 +0300

    added enhancements to logo and apollo client

--- frontend/src/lib/apollo.ts
+++ frontend/src/lib/apollo.ts
@@ -18,6 +18,53 @@ const authClient = new SetContextLink((prevContext) => {
     };
 });
 
+<<<<<<< Updated upstream
+=======
+// error
+const errorLink = new ErrorLink(({ error, operation, forward }) => {
+    console.log('errorLink triggered:', error);   // ← جديد
+  if (!CombinedGraphQLErrors.is(error)) {
+     console.log('Not a CombinedGraphQLErrors, skipping');   // ← جديد
+    return;
+  }
+
+  for (const err of error.errors) {
+    if (err.extensions?.code === "UNAUTHENTICATED") {
+      
+      return from(refreshAccessToken()).pipe(
+        switchMap((newToken) => {
+            console.log('Got new token:', newToken);
+          // حفظ التوكن الجديد
+          localStorage.setItem("accessToken", newToken);
+
+          // تعديل request القديم بالتوكن الجديد
+          operation.setContext(({ headers = {} }) => ({
+            headers: {
+              ...headers,
+              authorization: `Bearer ${newToken}`,
+            },
+          }));
+
+          // إعادة إرسال request القديم
+          return forward(operation);
+        }),
+
+        catchError(() => {
+          // refreshToken نفسه فشل
+          localStorage.removeItem("accessToken");
+
+          window.location.href = "/login";
+
+          return EMPTY;
+        })
+      );
+    }
+  }
+
+  return;
+});;
+
+>>>>>>> Stashed changes
 
 export const client = new ApolloClient({
     link: authClient.concat(httpLink),
